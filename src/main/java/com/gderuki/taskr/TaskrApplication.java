e48127b5762aa0aab36555d89959fb31410b8730
gderuki <paulgderuki@gmail.com>
2026-01-01 19:36:05 +0200

    Init spring boot

---
+++ src/main/java/com/gderuki/taskr/TaskrApplication.java
@@ -0,0 +1,13 @@
+package com.gderuki.taskr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskrApplication.class, args);
+    }
+
+}
